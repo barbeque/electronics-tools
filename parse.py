4aa5f2169875cf45a31172491ff54c7da2bf97dc
Mike <barbeque@users.noreply.github.com>
2023-11-15 20:49:35 -0700

    ESR judgment mode

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import csv, sys
 
 if len(sys.argv) < 3:
-	print(f'Usage: {sys.argv[0]} [uF] [V]')
+	print(f'Usage: {sys.argv[0]} [uF] [V] <esr>')
 	sys.exit(1)
 
 row = []
@@ -14,7 +14,14 @@ with open('machinist-esr.tsv', 'r') as f:
 capacitance = int(sys.argv[1])
 voltage = int(sys.argv[2])
 
+if(len(sys.argv) > 3):
+	esr = float(sys.argv[3])
+else:
+	esr = -1
+
 print('Searching for C =', capacitance,'V =', voltage)
+if(esr > 0):
+	print('Judging ESR=', esr)
 
 # find the column for voltage
 for i in range(0, len(row[0])):
@@ -34,7 +41,16 @@ for i in range(0, len(row) - 1):
 		if(len(maximum_esr) < 1):
 			print('No idea what the ESR for this pair is. Table is missing data...')
 		else:
+			maximum_esr = float(maximum_esr)
 			print('Max ESR=',maximum_esr)
+			if(esr > 0):
+				# judgment mode
+				if maximum_esr < esr:
+					print(f'Bad (max {maximum_esr})')
+				elif abs(maximum_esr - esr) < 0.3:
+					print(f'Marginal (max {maximum_esr})')
+				else:
+					print(f'Good (max {maximum_esr})')
 		break
 else:
 	print('Could not find capacitance value', capacitance, 'in this ESR table. It cannot yet interpolate.')
